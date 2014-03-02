6e47ab9fd03c8ddfdeb43003c437c7bba858c432
James Robinson <james.robinson@epii.co.uk>
2014-03-02 16:59:08 +0000

    Made minor modifications to files to fork georgecanning stub project

---
+++ src/main/java/uk/co/epii/conservatives/frederickrobinson/Main.java
@@ -0,0 +1,9 @@
+package uk.co.epii.conservatives.frederickrobinson;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Running Frederick Robinson");
+  }
+
+}
