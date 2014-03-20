2b87b63b82a4d7485de1c692b0dc14216fba5e16
jrrpl <jr@epii.co.uk>
2014-03-20 23:36:43 +0000

    Improved testing suite and changed code to catch found errorx

--- test/counterSpec.js
+++ test/counterSpec.js
@@ -1,59 +1,61 @@
 var expect = require("chai").expect
 var counter = require("../lib/counter.js");
 
-function mockTable() {
+function mockTable(array) {
   this.get = get;
   function get(id, postProcessing) {
-    return {};
+    if (id < array.length) {
+      postProcessing(false, array[id]);
+    }
+    else {
+      postProcessing("Not found", null);
+    }
   }
 }
 
 function mockDb() {
-  this.Count = new mockTable();
+  var overseeingReturn = 0;
+  this.Count = new mockTable(mockCounts);
+  var overseeing = new mockTable(mockOverseeings);
+  this.Overseeing = overseeing;
+  this.Overseeing.find = find;
+  function find(criteria, postProcessing) {
+    overseeing.get(criteria.POLLING_AREA, postProcessing);
+  }
+  this.PollingArea = new mockTable(mockPollingAreas);
 }
 
-counter.setDB(new mockDb());
+var mockCounts = [
+  {
+    "GUID": 0,
+    "provider": "gajusz@gmail.com",
+    "pollingArea": 0,
+    "password": "banana",
+    "votesCast": 2314,
+    "ballotBox": "Box 112",
+    "tallies": []
+  }
+];
+var mockOverseeings = [];
+var mockPollingAreas = [
+  {
+    "ID": 0,
+    "PARENT": null
+  },
+  {
+    "ID": 1,
+    "PARENT": 0
+  }
+];
 
-var count = {
-  "GUID": "01234-5678-90AB-CDEF",
-  "provider": "gajusz@gmail.com",
-  "pollingArea": 21,
-  "password": "banana",
-  "votesCast": 2314,
-  "ballotBox": "Box 112",
-  "tallies": [
-    {
-      "party": 148,
-      "candidate": 1,
-      "votes": 372
-    },
-    {
-      "party": 103,
-      "candidate": 2,
-      "votes": 59
-    },
-    {
-      "party": 153,
-      "candidate": 3,
-      "votes": 38
-    },
-    {
-      "party": 65,
-      "candidate": 4,
-      "votes": 116
-    },
-    {
-      "party": null,
-      "candidate": 5,
-      "votes":613
-    }
-  ]
-}
+var mockDb = new mockDb();
+
+counter.setDB(mockDb);
 
 describe("Counter", function() {
   describe("#Process()", function() {
     it("Should process Count", function() {
-      counter.process(count, function(err, old) {
+      counter.process(mockCounts[0], function(err, old) {
         
       });
     });
