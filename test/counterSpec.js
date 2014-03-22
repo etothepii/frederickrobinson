6fad5b940626aab9842d5a99cbc5b57ffa08a23e
jrrpl <jr@epii.co.uk>
2014-03-22 10:01:25 +0000

    Added check that updated and create are fired correctly

--- test/counterSpec.js
+++ test/counterSpec.js
@@ -2,15 +2,22 @@ var expect = require("chai").expect
 var counter = require("../lib/counter.js");
 
 function mockTable(array,table) {
+  this.created=0;
   this.get = get;
   function get(id, postProcessing) {
     if (id < array.length && array[id] != null) {
-      array[id].save = function(error) { }
+      array[id].saved=0;
+      array[id].save = function(error) { 
+        this.saved++;
+      }
       postProcessing(false, array[id]);
     }
     else {
       postProcessing("Not found in " + table, null);
     }
+  }
+  function create(obj, result) {
+    this.created++;
   }
 }
 
@@ -110,23 +117,39 @@ counter.setDB(mockDb);
 describe("Counter", function() {
   describe("#Process()", function() {
     it("Should process Count with single overseeing", function() {
+      mockCounts[0].saved = 0;
+      mockDb.Count.created = 0;
       counter.process(mockCounts[0], function(err, old) {
         expect(err).to.equal(false);
+        expect(mockCounts[0].saved).to.equal(0);
+        expect(mockDb.Count.created).to.equal(0);
       });
     });
     it("Should not process Count with two overseeing", function() {
+      mockCounts[1].saved = 0;
+      mockDb.Count.created = 0;
       counter.process(mockCounts[1], function(err, old) {
         expect(err).to.equal('Found multiple overseeing this area with this password failing');
+        expect(mockCounts[1].saved).to.equal(0);
+        expect(mockDb.Count.created).to.equal(0);
       });
     });
     it("Should not process Count with no one overseeing", function() {
+      mockCounts[2].saved = 0;
+      mockDb.Count.created = 0;
       counter.process(mockCounts[2], function(err, old) {
         expect(err).to.equal('No agent overseeing this count found with password provided');
+        expect(mockCounts[2].saved).to.equal(0);
+        expect(mockDb.Count.created).to.equal(0);
       });
     });
-    it("Should be able to process where only parent is overseen", function() {
+    it("Should be able to update where only parent is overseen", function() {
+      mockCounts[3].saved = 0;
+      mockDb.Count.created = 0;
       counter.process(mockCounts[3], function(err, old) {
         expect(err).to.equal(false);
+        expect(mockCounts[3].saved).to.equal(1);
+        expect(mockDb.Counter.created).to.equal(0);
       });
     });
   });
